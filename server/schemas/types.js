479f2e044e643bb1636fe9df0034ed5bac357e59
Tomoo Suzuki <solosoprani2002@gmail.com>
2020-04-25 09:07:38 +0900

    bug fix of graphql

--- server/schemas/types.js
+++ server/schemas/types.js
@@ -1,199 +1,41 @@
-//TODO:query毎に買い換える
 const graphql = require('graphql')
 
 const {
+  user,
+  account,
+  story,
+  post
+} = require("./models");
+
+const {
   GraphQLObjectType,
-  GraphQLID,
-  GraphQLString,
-  GraphQLBoolean,
-  GraphQLDate,
-  GraphQLSchema,
-  GraphQLInt,
-  GraphQLList,
-  GraphQLNonNull
 } = graphql
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: () => ({
-    id: {
-      type: GraphQLID
-    },
-    password: {
-      type: GraphQLString
-    },
-    user_type: {
-      type: GraphQLInt
-    },
-    magazine: {
-      type: GraphQLBoolean
-    },
-    agree_to_terms: {
-      type: GraphQLInt
-    }
-  })
+  // fields: () => (user)
+  fields: user
 })
 
 const AccountType = new GraphQLObjectType({
   name: 'Account',
-  fields: () => ({
-    id: {
-      type: GraphQLID
-    },
-    last_name: {
-      type: GraphQLString
-    },
-    first_name: {
-      type: GraphQLString
-    },
-    last_name_kana: {
-      type: GraphQLString
-    },
-    first_name_kana: {
-      type: GraphQLString
-    },
-    birthday: {
-      type: GraphQLDate
-    },
-    zip: {
-      type: GraphQLString
-    },
-    prefecture: {
-      type: GraphQLString
-    },
-    city: {
-      type: GraphQLString
-    },
-    address3: {
-      type: GraphQLString
-    },
-    building: {
-      type: GraphQLString
-    },
-    purpose: {
-      type: GraphQLString
-    }
-  })
+  // fields: () => (account)
+  fields: account
 })
 
 const StoryType = new GraphQLObjectType({
   name: 'Story',
-  fields: () => ({
-    id: {
-      type: GraphQLID
-    },
-    id_story: {
-      type: GraphQLID
-    },
-    title_main: {
-      type: GraphQLString
-    },
-    auther_name: {
-      type: GraphQLString
-    },
-    is_complete: {
-      type: GraphQLBoolean
-    },
-    genre: {
-      type: GraphQLBoolean
-    },
-    catch_copy: {
-      type: GraphQLString
-    },
-    synopsis: {
-      type: GraphQLString
-    },
-    keywords: {
-      type: GraphQLString
-    },
-    extreme_depiction: {
-      type: GraphQLString
-    },
-    color_type: {
-      type: GraphQLInt
-    },
-    accept_advertisement: {
-      type: GraphQLBoolean
-    },
-    accept_rating: {
-      type: GraphQLBoolean
-    },
-    accept_reviews: {
-      type: GraphQLBoolean
-    },
-    accept_impression: {
-      type: GraphQLBoolean
-    },
-    publish_evaluation: {
-      type: GraphQLBoolean
-    },
-    accept_typo_reports: {
-      type: GraphQLBoolean
-    }
-  })
+  // fields: () => (story)
+  fields: story
 })
 
 const PostType = new GraphQLObjectType({
   name: 'Post',
-  fields: () => ({
-    id_story: {
-      type: GraphQLID
-    },
-    id_post: {
-      type: GraphQLString
-    },
-    has_chapter: {
-      type: GraphQLString
-    },
-    title_chapter: {
-      type: GraphQLString
-    },
-    title: {
-      type: GraphQLString
-    },
-    story: {
-      type: GraphQLString
-    },
-    title_bk: {
-      type: GraphQLString
-    },
-    story_bk: {
-      type: GraphQLString
-    },
-    date_uploaded: {
-      type: GraphQLTimestamp
-    },
-    date_last_modify: {
-      type: GraphQLTimestamp
-    }
-  })
+  // fields: () => (post)
+  fields: post
 })
 
 exports.UserType = UserType;
 exports.AccountType = AccountType;
 exports.StoryType = StoryType;
 exports.PostType = PostType;
-
-
-// const DirectorType = new GraphQLObjectType({
-//   name: 'Director',
-//   fields: () => ({
-//     id: {
-//       type: GraphQLID
-//     },
-//     name: {
-//       type: GraphQLString
-//     },
-//     age: {
-//       type: GraphQLInt
-//     },
-//     movies: {
-//       type: new GraphQLList(MovieType),
-//       resolve(parent, args) {
-//         return Movie.find({
-//           directorId: parent.id
-//         })
-//       }
-//     }
-//   })
-// })
